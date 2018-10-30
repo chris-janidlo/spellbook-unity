e99b906cb4d8820c682e7c83df2f543b446f37d3
crassSandwich <christopherjanidlo@gmail.com>
2018-10-30 00:09:10 -0400

    Add pause script

---
+++ Input/PausedWhenActive.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+// pauses the game while the GameObject is active
+public class PausedWhenActive : MonoBehaviour
+{
+	[Tooltip("If this is true, pause when *inactive* instead of when active.")]
+	public bool Flip;
+
+	float previousTimeScale;
+
+	void OnEnable ()
+	{
+		setPauseState(!Flip);
+	}
+
+	void OnDisable ()
+	{
+		setPauseState(Flip);		
+	}
+
+	void setPauseState (bool value)
+	{
+		if (value)
+		{
+			previousTimeScale = Time.timeScale;
+			Time.timeScale = 0;
+		}
+		else
+		{
+			Time.timeScale = previousTimeScale;
+		}
+	}
+}
