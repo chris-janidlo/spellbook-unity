4502c55d1b1404c3cb2889af4b6d683bd5fb44a1
crassSandwich <christopherjanidlo@gmail.com>
2018-12-23 00:28:01 -0500

    Add ButtonIgnoreTransparent

---
+++ Input/ButtonIgnoreTransparent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+[RequireComponent(typeof(Image))]
+public class ButtonIgnoreTransparent : MonoBehaviour
+{
+    // minimum alpha a pixel must have to be considered a "hit"
+    // see https://docs.unity3d.com/ScriptReference/UI.Image-alphaHitTestMinimumThreshold.html
+    [Range(0, 1)]
+    public float AlphaThreshold = 1;
+
+    void Start()
+    {
+        GetComponent<Image>().alphaHitTestMinimumThreshold = 1;
+    }
+}
