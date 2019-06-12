081a983395e35863f2bf29e080536a355cdc9ccc
crass_sandwich <christopherjanidlo@gmail.com>
2019-06-11 23:39:57 -0400

    Add Floater

---
+++ ComponentUtilities/Floater2D.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace crass
+{
+public class Floater2D : MonoBehaviour
+{
+    [System.Serializable]
+    public class Sin
+    {
+        public float Period, Amplitude;
+        public float Offset { get; set; } = 0;
+
+        public float CurrentValue ()
+        {
+            return Amplitude * Mathf.Sin((2 * Mathf.PI / Period) * Time.time) + Offset;
+        }
+    }
+
+    public Sin VertWave, RotWave;
+
+    void Start ()
+    {
+        VertWave.Offset = transform.position.y;
+        RotWave.Offset = transform.rotation.eulerAngles.z;
+    }
+
+    void Update ()
+    {
+        transform.position = new Vector2(transform.position.x, VertWave.CurrentValue());
+
+        transform.rotation = Quaternion.Euler(Vector3.forward * RotWave.CurrentValue());
+    }
+}
+}
