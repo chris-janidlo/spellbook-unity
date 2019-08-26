58b9aec5435ae9d7a66f6cd645dc5064a7186930
crass_sandwich <christopherjanidlo@gmail.com>
2019-08-25 21:36:38 -0400

    Add PlayerPrefsEraser

---
+++ EditorWorkflowComponents/PlayerPrefsEraser.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace crass
+{
+public class PlayerPrefsEraser : MonoBehaviour
+{
+    [ContextMenu("NUCLEAR BUTTON")]
+    void erase ()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
+}
