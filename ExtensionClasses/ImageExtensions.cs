e47d915ca92bd2bc24caea01e314c42188f4f8ca
crassSandwich <christopherjanidlo@gmail.com>
2020-01-04 20:52:45 -0500

    Create extension classes for modifying common color values

---
+++ ExtensionClasses/ImageExtensions.cs
@@ -0,0 +1,35 @@
+using UnityEngine.UI;
+
+namespace crass
+{
+public static class ImageExtensions
+{
+	public static void SetR (this Image image, float r)
+	{
+		var color = image.color;
+		color.r = r;
+		image.color= color;
+	}
+
+	public static void SetG (this Image image, float g)
+	{
+		var color = image.color;
+		color.g = g;
+		image.color= color;
+	}
+
+	public static void SetB (this Image image, float b)
+	{
+		var color = image.color;
+		color.b = b;
+		image.color= color;
+	}
+
+	public static void SetA (this Image image, float a)
+	{
+		var color = image.color;
+		color.a = a;
+		image.color= color;
+	}
+}
+}
---
+++ ExtensionClasses/SpriteRendererExtensions.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace crass
+{
+public static class SpriteRendererExtensions
+{
+	public static void SetR (this SpriteRenderer SpriteRenderer, float r)
+	{
+		var color = SpriteRenderer.color;
+		color.r = r;
+		SpriteRenderer.color= color;
+	}
+
+	public static void SetG (this SpriteRenderer SpriteRenderer, float g)
+	{
+		var color = SpriteRenderer.color;
+		color.g = g;
+		SpriteRenderer.color= color;
+	}
+
+	public static void SetB (this SpriteRenderer SpriteRenderer, float b)
+	{
+		var color = SpriteRenderer.color;
+		color.b = b;
+		SpriteRenderer.color= color;
+	}
+
+	public static void SetA (this SpriteRenderer SpriteRenderer, float a)
+	{
+		var color = SpriteRenderer.color;
+		color.a = a;
+		SpriteRenderer.color= color;
+	}
+}
+}
