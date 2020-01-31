3d6d49680560b9c405213b81cef0931216bec798
crassSandwich <christopherjanidlo@gmail.com>
2020-01-30 23:03:56 -0500

    Add quick way to make TMPro color strings

---
+++ ExtensionClasses/StringExtensions.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public static class StringExtensions
+{
+	public static string WrapInTMProColorTag (this string input, Color color)
+	{
+        string rgba = ColorUtility.ToHtmlStringRGBA(color);
+        return $"<#{rgba}>{input}</color>";
+	}
+}
