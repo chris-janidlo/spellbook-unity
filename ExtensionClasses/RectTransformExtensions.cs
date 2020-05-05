9f47924f8f610f65c39a6089aa3fa0993e879cfa
crassSandwich <christopherjanidlo@gmail.com>
2020-05-04 21:28:42 -0400

    Add RectTransformExtensions class with SetPivot method

---
+++ ExtensionClasses/RectTransformExtensions.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace crass
+{
+public static class RectTransformExtensions
+{
+	// set pivot without changing the position of the element
+	// from http://answers.unity.com/answers/1545950/view.html
+	public static void SetPivot (this RectTransform rectTransform, Vector2 pivot)
+	{
+		Vector3 deltaPosition = rectTransform.pivot - pivot;
+
+		deltaPosition.Scale(rectTransform.rect.size);
+		deltaPosition.Scale(rectTransform.localScale);
+		deltaPosition = rectTransform.rotation * deltaPosition;
+
+		rectTransform.pivot = pivot;
+		rectTransform.localPosition -= deltaPosition;
+	}
+}
+}
