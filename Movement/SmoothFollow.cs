c2162365f6dffd0b3edbf70949e17725106f16aa
crassSandwich <christopherjanidlo@gmail.com>
2020-06-27 12:48:50 -0400

    Add rotational offset

--- Movement/SmoothFollow.cs
+++ Movement/SmoothFollow.cs
@@ -2,32 +2,36 @@ using UnityEngine;
 
 namespace crass
 {
-public class SmoothFollow : MonoBehaviour
-{
-	public Transform Target;
-	public Vector3 OffsetFromTarget;
+    public class SmoothFollow : MonoBehaviour
+    {
+        public Transform Target;
 
-	[Tooltip("How many seconds it takes for this object to reach the target's position plus the offset")]
-	public float MoveDelay = 0.3f;
-	
-	[Tooltip("How many seconds it takes for this object's rotation to reach the target's rotation")]
-	public float RotationDelay = 0.3f;
+        public Vector3 PositionOffsetFromTarget, RotationOffsetFromTarget;
 
-	Vector3 positionvelocity = Vector3.zero;
-	Quaternion rotationVelocity = Quaternion.identity;
+        [Tooltip("How many seconds it takes for this object to reach the target's position plus the offset")]
+        public float MoveDelay = 0.3f;
 
-	void Start ()
-	{
-		if (transform.parent != null)
-		{
-			transform.parent = null;
-		}
-	}
-	
-	void Update ()
-	{
-		transform.position = Vector3.SmoothDamp(transform.position, Target.TransformPoint(OffsetFromTarget), ref positionvelocity, MoveDelay);
-		transform.rotation = transform.rotation.SmoothDampTo(Target.rotation, ref rotationVelocity, RotationDelay);
-	}
-}
+        [Tooltip("How many seconds it takes for this object's rotation to reach the target's rotation")]
+        public float RotationDelay = 0.3f;
+
+        Vector3 positionvelocity = Vector3.zero;
+        Quaternion rotationVelocity = Quaternion.identity;
+
+        void Start ()
+        {
+            if (transform.parent != null)
+            {
+                transform.parent = null;
+            }
+        }
+
+        void Update ()
+        {
+            Vector3 followPosition = Target.TransformPoint(PositionOffsetFromTarget);
+            transform.position = Vector3.SmoothDamp(transform.position, followPosition, ref positionvelocity, MoveDelay);
+
+            Quaternion followRotation = Target.rotation * Quaternion.Euler(RotationOffsetFromTarget);
+            transform.rotation = transform.rotation.SmoothDampTo(followRotation, ref rotationVelocity, RotationDelay);
+        }
+    }
 }
