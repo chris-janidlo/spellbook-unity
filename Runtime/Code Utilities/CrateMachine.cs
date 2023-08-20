6a1ab04efc542f6d031ea1eb588d59d72e777348
Chris Janidlo <christopherjanidlo@gmail.com>
2023-08-20 14:15:30 -0500

    CrateMachine changes:
    * Apply CSharpier
    * Set CrateMachineDriver's transform parent to the CrateMachine's parent
    * Only run CrateMachine (Fixed)Update if parent is enabled

--- Runtime/Code Utilities/CrateMachine.cs
+++ Runtime/Code Utilities/CrateMachine.cs
@@ -4,149 +4,160 @@ using UnityEngine;
 
 namespace crass
 {
-// necessary in order to have a non-generic CrateMachineDriver class (since generic MonoBehaviours aren't supported)
-public abstract class ACrateMachine
-{
-    internal abstract void Process(CrateMachineProcessType processType);
-}
-
-internal enum CrateMachineProcessType
-{
-    Update,
-    FixedUpdate
-}
-
-internal class CrateMachineDriver : MonoBehaviour
-{
-    private ACrateMachine crateMachine;
-
-    public void Initialize(ACrateMachine crateMachine)
+    // necessary in order to have a non-generic CrateMachineDriver class (since generic MonoBehaviours aren't supported)
+    public abstract class ACrateMachine
     {
-        this.crateMachine = crateMachine;
+        internal abstract void Process(CrateMachineProcessType processType);
     }
 
-    void Update()
+    internal enum CrateMachineProcessType
     {
-        crateMachine.Process(CrateMachineProcessType.Update);
+        Update,
+        FixedUpdate
     }
 
-    void FixedUpdate()
+    internal class CrateMachineDriver : MonoBehaviour
     {
-        crateMachine.Process(CrateMachineProcessType.FixedUpdate);
-    }
-}
+        private ACrateMachine crateMachine;
+        private MonoBehaviour parent;
 
-public class CrateMachine<TParent> : ACrateMachine
-{
-    private readonly TParent parent;
-    private readonly Dictionary<Type, Crate<TParent>> crates = new();
-    private Crate<TParent> currentCrate;
-    private Type initialCrateType;
-    private GameObject driver;
-
-    public CrateMachine(TParent parent)
-    {
-        this.parent = parent;
-    }
-
-    public CrateMachine<TParent> AddCrate<TCrate>(TCrate crate, bool initial = false) where TCrate : Crate<TParent>
-    {
-        crates[typeof(TCrate)] = crate;
-        crate.SetParent(parent);
-        if (initial) SetInitialCrate<TCrate>();
-        return this;
-    }
-
-    public CrateMachine<TParent> SetInitialCrate<TCrate>() where TCrate : Crate<TParent>
-    {
-        initialCrateType = typeof(TCrate);
-        return this;
-    }
-
-    public CrateMachine<TParent> Build()
-    {
-        var machineType = $"CrateMachine<{typeof(TParent).Name}>";
-        var parentName = parent.ToString();
-
-        if (crates.Count == 0)
-            throw new InvalidOperationException($"{machineType} for {parentName} has no crates");
-
-        if (initialCrateType == null)
-            throw new InvalidOperationException($"{machineType} for {parentName} has no initial crate set");
-
-        if (driver != null)
-            throw new InvalidOperationException($"{machineType} is already built for {parentName}");
-
-        currentCrate = crates[initialCrateType];
-        driver = new GameObject($"{parentName} driver (for {machineType})");
-        driver.AddComponent<CrateMachineDriver>().Initialize(this);
-        return this;
-    }
-
-    public void CustomCall<TInterface>(Action<TInterface> call)
-    {
-        if (currentCrate is TInterface @interface)
-            call(@interface);
-    }
-
-    internal override void Process(CrateMachineProcessType processType)
-    {
-        switch (processType)
+        public void Initialize(ACrateMachine crateMachine, MonoBehaviour parent)
         {
-            case CrateMachineProcessType.Update:
-                currentCrate.OnUpdate();
-                break;
+            this.crateMachine = crateMachine;
+            this.parent = parent;
 
-            case CrateMachineProcessType.FixedUpdate:
-                currentCrate.OnFixedUpdate();
-                break;
-
-            default:
-                throw new ArgumentException(processType.ToString());
+            transform.parent = parent.transform;
+            transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
         }
 
-        DoTransition(currentCrate.GetTransition());
+        void Update()
+        {
+            if (parent != null && parent.enabled)
+                crateMachine.Process(CrateMachineProcessType.Update);
+        }
+
+        void FixedUpdate()
+        {
+            if (parent != null && parent.enabled)
+                crateMachine.Process(CrateMachineProcessType.FixedUpdate);
+        }
     }
 
-    private void DoTransition(Type newCrateType)
+    public class CrateMachine<TParent> : ACrateMachine
+        where TParent : MonoBehaviour
     {
-        if (newCrateType is null) return;
+        private readonly TParent parent;
+        private readonly Dictionary<Type, Crate<TParent>> crates = new();
+        private Crate<TParent> currentCrate;
+        private Type initialCrateType;
+        private GameObject driver;
 
-        currentCrate.OnExit();
-        currentCrate = crates[newCrateType];
-        currentCrate.OnEnter();
+        public CrateMachine(TParent parent)
+        {
+            this.parent = parent;
+        }
+
+        public CrateMachine<TParent> AddCrate<TCrate>(TCrate crate, bool initial = false)
+            where TCrate : Crate<TParent>
+        {
+            crates[typeof(TCrate)] = crate;
+            crate.SetParent(parent);
+            if (initial)
+                SetInitialCrate<TCrate>();
+            return this;
+        }
+
+        public CrateMachine<TParent> SetInitialCrate<TCrate>()
+            where TCrate : Crate<TParent>
+        {
+            initialCrateType = typeof(TCrate);
+            return this;
+        }
+
+        public CrateMachine<TParent> Build()
+        {
+            var machineType = $"CrateMachine<{typeof(TParent).Name}>";
+            var parentName = parent.ToString();
+
+            if (crates.Count == 0)
+                throw new InvalidOperationException(
+                    $"{machineType} for {parentName} has no crates"
+                );
+
+            if (initialCrateType == null)
+                throw new InvalidOperationException(
+                    $"{machineType} for {parentName} has no initial crate set"
+                );
+
+            if (driver != null)
+                throw new InvalidOperationException(
+                    $"{machineType} is already built for {parentName}"
+                );
+
+            currentCrate = crates[initialCrateType];
+            driver = new GameObject($"{parentName} driver (for {machineType})");
+            driver.AddComponent<CrateMachineDriver>().Initialize(this, parent);
+            return this;
+        }
+
+        public void CustomCall<TInterface>(Action<TInterface> call)
+        {
+            if (currentCrate is TInterface @interface)
+                call(@interface);
+        }
+
+        internal override void Process(CrateMachineProcessType processType)
+        {
+            switch (processType)
+            {
+                case CrateMachineProcessType.Update:
+                    currentCrate.OnUpdate();
+                    break;
+
+                case CrateMachineProcessType.FixedUpdate:
+                    currentCrate.OnFixedUpdate();
+                    break;
+
+                default:
+                    throw new ArgumentException(processType.ToString());
+            }
+
+            DoTransition(currentCrate.GetTransition());
+        }
+
+        private void DoTransition(Type newCrateType)
+        {
+            if (newCrateType is null)
+                return;
+
+            currentCrate.OnExit();
+            currentCrate = crates[newCrateType];
+            currentCrate.OnEnter();
+        }
     }
-}
 
-public abstract class Crate<TParent>
-{
-    // ReSharper disable once InconsistentNaming
-    protected TParent C;
-
-    public void SetParent(TParent parent)
+    public abstract class Crate<TParent>
+        where TParent : MonoBehaviour
     {
-        C = parent;
-    }
+        // ReSharper disable once InconsistentNaming
+        protected TParent C;
 
-    public virtual void OnEnter()
-    {
-    }
+        public void SetParent(TParent parent)
+        {
+            C = parent;
+        }
 
-    public virtual void OnExit()
-    {
-    }
+        public virtual void OnEnter() { }
 
-    public virtual Type GetTransition()
-    {
-        return null;
-    }
+        public virtual void OnExit() { }
 
-    public virtual void OnUpdate()
-    {
-    }
+        public virtual Type GetTransition()
+        {
+            return null;
+        }
 
-    public virtual void OnFixedUpdate()
-    {
+        public virtual void OnUpdate() { }
+
+        public virtual void OnFixedUpdate() { }
     }
-}
 }
