a5b9c9d426417a68146ad21ae6fa22ca673b4314
Chris Janidlo <christopherjanidlo@gmail.com>
2023-09-05 18:58:20 -0500

    Remove Vector3Int.Normalize
    Not sure what it was intended to do

--- Runtime/Extension Classes/VectorIntExtensions.cs
+++ Runtime/Extension Classes/VectorIntExtensions.cs
@@ -2,30 +2,20 @@ using UnityEngine;
 
 namespace crass
 {
-public static class VectorIntExtensions
-{
-	public static Vector2Int DivideBy (this Vector2Int dividend, float divisor)
-	{
-		return new Vector2Int
-		(
-			(int) (dividend.x / divisor), 
-			(int) (dividend.y / divisor)
-		);
-	}
+    public static class VectorIntExtensions
+    {
+        public static Vector2Int DivideBy(this Vector2Int dividend, float divisor)
+        {
+            return new Vector2Int((int)(dividend.x / divisor), (int)(dividend.y / divisor));
+        }
 
-	public static Vector3Int DivideBy (this Vector3Int dividend, float divisor)
-	{
-		return new Vector3Int
-		(
-			(int) (dividend.x / divisor),
-			(int) (dividend.y / divisor),
-			(int) (dividend.z / divisor)
-		);
-	}
-
-	public static Vector3Int Normalize (this Vector3Int vector)
-	{
-		return Vector3Int.RoundToInt(((Vector3) vector).normalized);
-	}
-}
+        public static Vector3Int DivideBy(this Vector3Int dividend, float divisor)
+        {
+            return new Vector3Int(
+                (int)(dividend.x / divisor),
+                (int)(dividend.y / divisor),
+                (int)(dividend.z / divisor)
+            );
+        }
+    }
 }
